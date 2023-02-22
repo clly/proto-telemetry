8c150b252d13d369343b85d4b25a2906919a80a4
Connor Kelly <connor.r.kelly@gmail.com>
2023-02-21 22:43:45 -0600

    Add opencensus as a backend

--- cmd/pkg/generators/generator.go
+++ cmd/pkg/generators/generator.go
@@ -8,7 +8,7 @@ import (
 )
 
 type generator interface {
-	Generate(g *protogen.GeneratedFile, named bool)
+	Generate(f *FileGenerator, named bool)
 }
 
 type Generator[T generator] struct {
@@ -25,11 +25,13 @@ func NewMapGenerator(m *protogen.Field) generator {
 	}
 }
 
-func (m *MapGenerator) Generate(g *protogen.GeneratedFile, named bool) {
+func (m *MapGenerator) Generate(f *FileGenerator, named bool) {
+	g := f.g
 	var key = fmt.Sprintf("%s.%s", strings.ToLower(m.m.Parent.GoIdent.GoName), strings.ToLower(m.m.GoName))
 	if named {
 		key = fmt.Sprintf("pfx.%s", key)
 	}
+
 	g.P("for m, v := range x.Get", m.m.GoName, "() {")
 	g.P("span.SetAttributes(")
 	// g.P(`attribute.String("`, strings.ToLower(m.m.Parent.GoIdent.GoName), `.`, strings.ToLower(m.m.GoName), `_%s, x.Msg),`)
