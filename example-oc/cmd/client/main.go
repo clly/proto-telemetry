d4c6b2e9bf976bc344f9ae5d7baaca47c0031572
Connor Kelly <connor.r.kelly@gmail.com>
2023-02-26 23:50:17 -0600

    Create opencensus example

---
+++ example-oc/cmd/client/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.opentelemetry.io/otel"
+	"google.golang.org/grpc"
+
+	echov1 "github.com/clly/proto-telemetry/example-otel/gen/proto/go/echo/v1"
+	"github.com/clly/proto-telemetry/example-otel/tracing"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	connectTo := "127.0.0.1:8080"
+	conn, err := grpc.Dial(connectTo, grpc.WithBlock(), grpc.WithInsecure())
+	if err != nil {
+		return fmt.Errorf("failed to connect to echo service at %s: %w", connectTo, err)
+	}
+	log.Println("connected to", connectTo)
+
+	shutdown, err := tracing.Init()
+	if err != nil {
+		return err
+	}
+	defer shutdown()
+
+	echo := echov1.NewEchoServiceClient(conn)
+
+	ctx := context.Background()
+	ctx, span := otel.Tracer("protoc-gen-go-telemetry/example/client").Start(ctx, "Echo Client")
+	defer span.End()
+
+	req := &echov1.EchoRequest{
+		Msg: "Hello World!",
+	}
+	req.TraceAttributes(ctx)
+	if _, err := echo.Echo(context.Background(), &echov1.EchoRequest{
+		Msg: "Hello World!",
+	}); err != nil {
+		return fmt.Errorf("failed to Echo: %w", err)
+	}
+
+	log.Println("Successfully echoed")
+	return nil
+}
---
+++ example-oc/cmd/server/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"google.golang.org/genproto/googleapis/type/datetime"
+	"google.golang.org/grpc"
+
+	echov1 "github.com/clly/proto-telemetry/example-otel/gen/proto/go/echo/v1"
+	"github.com/clly/proto-telemetry/example-otel/tracing"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	listen := "127.0.0.1:8080"
+	listener, err := net.Listen("tcp", listen)
+	if err != nil {
+		return fmt.Errorf("failed to listen on %s: %w", listen, err)
+	}
+
+	shutdown, err := tracing.Init()
+	if err != nil {
+		return err
+	}
+	defer shutdown()
+
+	server := grpc.NewServer()
+	echov1.RegisterEchoServiceServer(server, &svr{})
+	log.Println("listening on", listener.Addr())
+	if err := server.Serve(listener); err != nil {
+		return fmt.Errorf("failed to server grpc server: %w", err)
+	}
+
+	return nil
+}
+
+type svr struct {
+	echov1.UnimplementedEchoServiceServer
+}
+
+func (s *svr) Echo(ctx context.Context, req *echov1.EchoRequest) (*echov1.EchoResponse, error) {
+	ctx, span := otel.Tracer("protoc-gen-go-telemetry/example/server").Start(ctx, "Echo")
+	defer span.End()
+	req.TraceAttributes(ctx)
+	return &echov1.EchoResponse{
+		Msg: req.Msg,
+		Now: dtFromTime(time.Now()),
+	}, nil
+}
+
+func dtFromTime(t time.Time) *datetime.DateTime {
+	return &datetime.DateTime{
+		Year:    int32(t.Year()),
+		Month:   int32(t.Month()),
+		Day:     int32(t.Day()),
+		Hours:   int32(t.Hour()),
+		Minutes: int32(t.Minute()),
+		Seconds: int32(t.Second()),
+		Nanos:   int32(t.Nanosecond()),
+	}
+}
